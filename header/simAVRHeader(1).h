49eb42ae57c6ec341d54d7a026dd60b670424895
Andrew Bazua <abazu001@wch136-12.cs.ucr.edu>
2019-10-07 12:38:06 -0700

    Completed Exercise 1

--- header/simAVRHeader(1).h
+++ header/simAVRHeader(1).h
@@ -5,9 +5,9 @@
 #endif
 
 #include <avr/sleep.h>
-#include "include/simavr/avr/avr_mcu_section.h"
+#include "include/home/paper_crane/Downloads/simavr/simavr/sim/avr/avr_mcu_section.h"
 AVR_MCU(F_CPU,"atmega1284");
-AVR_MCU_VCD_FILE("build/results/Lab3_bitManipulation(1)_trace.vcd",1000);
+AVR_MCU_VCD_FILE("build/results/lab3_bitManipulation_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
